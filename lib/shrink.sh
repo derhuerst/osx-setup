0b3758df8860935ee660d3c0e690fc181e411343
Jannis R <mail@jannisr.de>
2015-10-02 00:42:28 +0200

    `shrink.sh`: adapted to El Capitan

--- lib/shrink.sh
+++ lib/shrink.sh
@@ -50,6 +50,34 @@ if ask 'Delete Grapher example documents?'; then
 	fi
 fi
 
+if ask 'Delete Game Center.app?'; then
+	sudo rm -rf '/Applications/Game Center.app'
+fi
+
+if ask 'Delete Stickies.app?'; then
+	sudo rm -rf '/Applications/Stickies.app'
+fi
+
+if ask 'Delete Chess.app?'; then
+	sudo rm -rf /Applications/Chess.app
+fi
+
+if ask 'Delete iBooks.app?'; then
+	sudo rm -rf /Applications/iBooks.app
+fi
+
+if ask 'Delete Audio Midi Setup.app?'; then
+	sudo rm -rf '/Applications/Utilities/Audio Midi Setup.app'
+fi
+
+if ask 'Delete Boot Camp Assistant.app?'; then
+	sudo rm -rf '/Applications/Utilities/Boot Camp Assistant.app'
+fi
+
+if ask 'Delete Migration Assistant.app?'; then
+	sudo rm -rf '/Applications/Utilities/Migration Assistant.app'
+fi
+
 if ask 'Delete every voice except Alex?'; then
 	sudo rm -rf /System/Library/Speech/Voices/!(Alex.speechVoice)
 	if ask 'Delete Alex as well?'; then
@@ -89,29 +117,28 @@ if ask 'Delete mecabra from BSD (for Chinese and Korean)?'; then
 	sudo rm -rf /usr/lib/libmecabra.dylib
 fi
 
-if ask 'Delete Kotoeri input method (for Japanese)?'; then
-	sudo rm -rf /System/Library/Input\ Methods/Kotoeri.app
+if ask 'Delete Ainu input method (for Japanese)?'; then
+	sudo rm -rf /System/Library/Input\ Methods/AinuIM.app
 fi
 
-if ask 'Delete KoreanIM input method?'; then
+if ask 'Delete Hindi input method?'; then
+	sudo rm -rf /System/Library/Input\ Methods/HindiIM.app
+fi
+
+if ask 'Delete Japanese input method?'; then
+	sudo rm -rf /System/Library/Input\ Methods/JapaneseIM.app
+fi
+
+if ask 'Delete Korean input method?'; then
 	sudo rm -rf /System/Library/Input\ Methods/KoreanIM.app
 fi
 
-if ask 'Delete VietnameseIM input method?'; then
-	sudo rm -rf /System/Library/Input\ Methods/VietnameseIM.app
-fi
-
-if ask 'Delete TamilIM input method?'; then
+if ask 'Delete Tamil input method?'; then
 	sudo rm -rf /System/Library/Input\ Methods/TamilIM.app
 fi
 
-if ask 'Delete Python 2.3?'; then
-	sudo rm -rf /System/Library/Frameworks/Python.framework/Versions/2.3
-	sudo rm -rf /Library/Python/2.3
-fi
-
-if ask 'Delete AirPort Utility.app?'; then
-	sudo rm -rf /Applications/Utilities/AirPort\ Utility.app
+if ask 'Delete Vietnamese input method?'; then
+	sudo rm -rf /System/Library/Input\ Methods/VietnameseIM.app
 fi
 
 if ask 'Add /bin and /sbin to Spotlight?'; then
@@ -121,22 +148,6 @@ fi
 
 if ask 'Add /Library to Spotlight?'; then
 	mdimport /Library
-fi
-
-if ask 'Delete Game Center.app?'; then
-	sudo rm -rf /Applications/Game\ Center.app
-fi
-
-if ask 'Delete iBooks.app?'; then
-	sudo rm -rf /Applications/iBooks.app
-fi
-
-if ask 'Delete Stickies.app?'; then
-	sudo rm -rf /Applications/Stickies.app
-fi
-
-if ask 'Delete Chess.app?'; then
-	sudo rm -rf /Applications/Chess.app
 fi
 
 if ask 'Disable hibernation?'; then
